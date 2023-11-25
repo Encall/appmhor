26474fa8df6562b71629d452a7c1d2b3a0d6db76
SSSBoOm <boom1909chanapat@gmail.com>
2023-11-25 13:41:09 +0700

    feat: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SignUpPage from "./pages/signup";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SignUpPage from './pages/signup'
+import { Navbar } from './components/navbar'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
