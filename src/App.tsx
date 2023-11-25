698553c004796dedd27585883fadb046a6a6ca7d
Inteoty <kittipong.tpy@gmail.com>
2023-11-25 17:15:17 +0700

    myHealth Page
    Co-authored-by: Wongsatorn Sungsilpawech <wongsatorn.sung@hotmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/signup";
 import { Home } from "./pages/home";
+import { MyHealth } from "./pages/myHealth";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/myHealth" element={<MyHealth />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </BrowserRouter>
